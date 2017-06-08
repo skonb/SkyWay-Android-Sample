31b8dc84ee935ccee89d7bd704051b3e2dd77733
Shunpei Kobayashi <skonb@me.com>
2017-06-09 04:58:39 +0900

    Pending

--- app/src/main/java/io/skyway/testpeerjava/MediaActivity.java
+++ app/src/main/java/io/skyway/testpeerjava/MediaActivity.java
@@ -32,7 +32,23 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttClientStatusCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttManager;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttMessageDeliveryCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
+import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
+import com.amazonaws.regions.Regions;
+import com.github.bassaer.chatmessageview.models.Message;
+import com.github.bassaer.chatmessageview.models.User;
+import com.github.bassaer.chatmessageview.views.ChatView;
+
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 
 
 /**
@@ -55,18 +71,150 @@ public class MediaActivity
     private boolean _bCalling;
     private boolean _videoEnabled = true;
     private boolean _audioEnabled = true;
+    private ChatView mChatView;
+    private AWSIotMqttManager _mqttManager;
+    private static String ENDPOINT = "a3xyvz7ca0wh8.iot.ap-northeast-1.amazonaws.com";
+    private static String IDENTITY_POOL_ID = "ap-northeast-1:8848275d-4dbc-4acb-a0ff-2c70acb57760";
+    private CognitoCachingCredentialsProvider _credentialsProvider;
+
+    private String _topic;
+    private User _myChatUser = new User(0, "", null);
+    private User _peerChatUser = new User(1, "", null);
+
+
+    protected void initializeMQTTSession(String id) {
+        if (_mqttManager == null) {
+            _mqttManager = new AWSIotMqttManager(id, ENDPOINT);
+            _mqttManager.connect(_credentialsProvider, new AWSIotMqttClientStatusCallback() {
+                @Override
+                public void onStatusChanged(AWSIotMqttClientStatus status, Throwable throwable) {
+
+                }
+            });
+
+        }
+    }
+
+    public void updateTopic(String newTopic) {
+        if (_topic != null) {
+            endListeningMQTTTopic();
+        }
+        _topic = newTopic;
+    }
+
+    protected void startListeningMQTTTopic() {
+        if (_mqttManager != null) {
+            _mqttManager.subscribeToTopic(_topic, AWSIotMqttQos.QOS0, new AWSIotMqttNewMessageCallback() {
+                @Override
+                public void onMessageArrived(String topic, byte[] data) {
+                    try {
+                        String str = new String(data, "UTF-8");
+                        JSONObject json = new JSONObject(str);
+                        String from = json.getString("from");
+                        if (!from.equals(_id)) {
+                            String messageStr = json.getString("message");
+                            final Message message = new Message.Builder()
+                                    .setUser(_peerChatUser)
+                                    .setRightMessage(false)
+                                    .setMessageText(messageStr)
+                                    .build();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mChatView.receive(message);
+                                }
+                            });
+                        }
+                    } catch (JSONException e) {
+
+                    } catch (UnsupportedEncodingException e) {
+
+
+                    }
+                }
+            });
+        }
+    }
+
+    protected void endListeningMQTTTopic() {
+        if (_mqttManager != null) {
+            _mqttManager.unsubscribeTopic(_topic);
+        }
+    }
+
+    protected void sendMessage(String message) {
+        if (_mqttManager != null) {
+            JSONObject json = new JSONObject();
+            try {
+                json.put("from", _id);
+                json.put("message", message);
+            } catch (JSONException e) {
+
+            }
+            _mqttManager.publishString(json.toString(), _topic, AWSIotMqttQos.QOS0, new AWSIotMqttMessageDeliveryCallback() {
+                @Override
+                public void statusChanged(MessageDeliveryStatus status, Object userData) {
+                    switch (status) {
+                        case Success: {
+                            String messageStr = (String) userData;
+                            final Message message = new Message.Builder()
+                                    .setUser(_myChatUser)
+                                    .setRightMessage(true)
+                                    .setMessageText(messageStr)
+                                    .build();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mChatView.send(message);
+                                    mChatView.setInputText(null);
+                                }
+                            });
+                        }
+                        break;
+                        case Fail:
+                            break;
+                    }
+                }
+            }, message);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
         Window wnd = getWindow();
         wnd.addFlags(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_video_chat);
 
+        View v = findViewById(android.R.id.content);
+        if (v != null) {
+            v.setKeepScreenOn(true);
+        }
+
         _handler = new Handler(Looper.getMainLooper());
         Context context = getApplicationContext();
+
+        _credentialsProvider = new CognitoCachingCredentialsProvider(
+                getApplicationContext(),
+                IDENTITY_POOL_ID, // Identity Pool ID
+                Regions.AP_NORTHEAST_1 // Region
+        );
+
+
+        mChatView = (ChatView) findViewById(R.id.chat_view);
+
+        mChatView.setOnClickSendButtonListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String messageStr = mChatView.getInputText();
+                sendMessage(messageStr);
+            }
+
+        });
 
         //////////////////////////////////////////////////////////////////////
         //////////////////  START: Initialize SkyWay Peer ////////////////////
@@ -219,6 +367,10 @@ public class MediaActivity
             _bCalling = true;
         }
 
+        if (_bCalling) {
+            updateTopic(_id);
+        }
+
         //////////////////////////////////////////////////////////////////////
         /////////////////// END: Calling SkyWay Peer   ///////////////////////
         //////////////////////////////////////////////////////////////////////
@@ -243,6 +395,7 @@ public class MediaActivity
 
                 if (object instanceof String) {
                     _id = (String) object;
+                    initializeMQTTSession(_id);
                     Log.d(TAG, "ID:" + _id);
 
                     updateUI();
@@ -267,6 +420,10 @@ public class MediaActivity
 
                 _bCalling = true;
 
+                updateTopic(_media.peer);
+                startListeningMQTTTopic();
+
+
                 updateUI();
             }
         });
@@ -276,6 +433,7 @@ public class MediaActivity
             @Override
             public void onCallback(Object object) {
                 Log.d(TAG, "[On/Close]");
+
             }
         });
 
@@ -484,6 +642,8 @@ public class MediaActivity
 
         if (null != _media) {
             _media.close();
+            _mqttManager.unsubscribeTopic(_topic);
+            _topic = null;
         }
     }
 
